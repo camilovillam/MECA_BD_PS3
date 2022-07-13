40cd5b7d37d5a5e398b66c5369029e4a07d697ab
Camilo Villa <camilo.villa.moreno@gmail.com>
2022-07-13 18:44:15 -0500

    Update E12_PS3.R

---
+++ scripts/E12_PS3.R
@@ -0,0 +1,55 @@
+#Big Data and Machine Learning for Applied Economics
+#MEcA - Uniandes
+#Problem Set 3
+#
+#Equipo 12
+
+#Jorge E. García
+#Ingrid Lorena Molano
+#Camilo Villa Moreno
+
+#Julio 26, 2022
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 0. PRELIMINARES: PREPARACIÓN ESPACIO DE TRABAJO Y LIBRERÍAS----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+##Limpiar el entorno ----
+rm(list=ls())
+
+
+##Carga de librerías ----
+install.packages("pacman")
+library(pacman)
+
+p_load(rio,
+       doParallel,
+       gtsummary,
+       GGally,
+       stargazer,
+       fabricatr,
+       tableone,
+       arsenal,
+       janitor,
+       tidyverse,
+       gamlr,
+       skimr, 
+       caret,
+       rvest,
+       stargazer,
+       smotefamily,
+       MASS,
+       ROCR,
+       pROC,
+       rpart,
+       rpart.plot,
+       glmnet,
+       xgboost)
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 1. CARGUE DE LAS BASES DE DATOS Y EXPLORACIÓN INICIAL----
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
