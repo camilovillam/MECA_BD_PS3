848967faca1b16eef06100f753f7fd946abca43c
Camilo Villa <camilo.villa.moreno@gmail.com>
2022-07-17 09:31:08 -0500

    Carga de datos y exploración inicial (preliminar)

--- scripts/E12_PS3.R
+++ scripts/E12_PS3.R
@@ -53,3 +53,57 @@ p_load(rio,
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+setwd("~/GitHub/MECA_BD_PS3")
+
+test_prop <-readRDS("./stores/test.rds")
+train_prop <-readRDS("./stores/train.rds")
+
+
+
+##1.2. Exploración incial de los datos ----
+
+##Exploración de las bases de datos:
+skim(train_prop)
+skim(test_prop)
+
+#Comparación de variables (columnas) entre las dos bases de datos:
+compare_df_cols(train_prop, test_prop)
+
+#Resumen de diferencias
+all_equal(train_prop, test_prop)
+
+
+# Las variables son las mismas, excepto el Asking Price (Y) que no está
+# en la de Test (esperable).
+# 
+# De acuerdo con Skim, las variables con mayor cantidad de NAs son:
+#
+
+
+# Var		        Complete Rate (Train)		    Complete rate (test)
+# -------------------------------------------------------------------------
+# rooms  		 	      0.502				            0.515			
+# bathrooms 	  	  0.720				            0.617
+# surface_total 	  0.258				            0.185      
+# surface_covered   0.188				            0.103
+# description       0.999	(70 NA)		      	0.999 	(16 NA)
+# title             1.00 	(35 NA)        		1 	    (0 NA)
+
+
+#surface_total, surface covered, rooms son prácticamente inutilizables.
+#Hay que revisar si en la descripción, por análisis de texto, está la info.
+
+
+#Se deben completar mínimo 2 variables geográficas:
+
+#Ej. Distancia estación Metro / Transmilenio / Parada de bus(?).
+#Ej. Distancia al centro de la ciudad.
+
+
+#Se deben agregar mínimo dos variables por procesamiento de texto a partir de la descripción:
+# Ej: Disponibilidad ascensor
+# Ej: Parqueaderos, número de parqueaderos
+# Ej: Área total, superficie (ver NAs, comentarios anteriores).
+
+
+
